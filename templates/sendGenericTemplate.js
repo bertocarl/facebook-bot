da5cfd53068a5e485e76273ee864266eb9cd2a50
Albert Omware <aomware@gmail.com>
2019-07-24 17:14:09 +0300

    removed files from gitignore

---
+++ templates/sendGenericTemplate.js
@@ -0,0 +1,36 @@
+const request = require('request');
+
+module.exports = function sendGenericTemplate(recipientId, respBody) {
+    console.log(respBody);
+    const messageValue = [];
+    for (let i = 0; i < respBody.length; i++) { // I dont like using forEach
+        let obj = {
+            "title":respBody[i].message_title,
+            "contact_id": respBody[i].contact_id,
+            "text": respBody[i].text,
+        }
+        messageValue.push(obj);
+    }
+    let messageData = {
+        "attachment": {
+            "type": "template",
+            "payload": {
+                "template_type": "generic",
+                "elements": messageValue
+            }
+        }
+    }
+    request({
+        url: 'https://graph.facebook.com/v2.6/me/messages',
+            qs: { access_token: process.env.PAGE_ACCESS_TOKEN },
+            method: 'POST',
+            json: {
+                recipient: {id: recipientId},
+                message: messageData,
+            }
+    }, function(error, response, body){
+        if (error) {
+            console.log("Error sending message: " + response.error)
+        }
+    })
+}
